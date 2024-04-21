2d0f161417ad76d7e2977b1c228c67101842af66
Nuked <nuked8@gmail.com>
2024-04-21 21:59:33 +0200

    forgot to delete this

--- custom_test/custom_test.js
+++
@@ -1,8 +0,0 @@
-//console.log(LiteGraph.registered_node_types);
-
-//const menuOptions = document.getElementById('menu-options');
-
-new_menu_options.push("Add to category");
-new_submenu_options.push(["Category 1", "Category 2"]);
-
-console.log("custom loaded")
